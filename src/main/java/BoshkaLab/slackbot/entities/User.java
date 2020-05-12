33093ba663ffd496abe26137da0fcaf9e3adcfd3
Maxim Zadumin <recentfear@yandex.ru>
2020-05-12 20:01:50 +0500

    Add User and UserType entities

--- src/main/java/BoshkaLab/slackbot/entities/User.java
+++ src/main/java/BoshkaLab/slackbot/entities/User.java
@@ -1,4 +1,70 @@
 package BoshkaLab.slackbot.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String login;
+    private String password;
+    private String email;
+    private String name;
+    private String surname;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="usertype_id", referencedColumnName = "id")
+    private UserType type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    
 }
--- src/main/java/BoshkaLab/slackbot/entities/UserType.java
+++ src/main/java/BoshkaLab/slackbot/entities/UserType.java
@@ -1,4 +1,38 @@
 package BoshkaLab.slackbot.entities;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class UserType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(
+            mappedBy = "userType",
+            cascade = CascadeType.PERSIST,
+            fetch = FetchType.LAZY
+    )
+    private Set<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
